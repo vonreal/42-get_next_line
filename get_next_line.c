1ee2f771ecd4cf6788f81f062d1e2619dfd3496b
vonrea <vonreal20@gmail.com>
2021-01-22 03:10:44 +0900

    BUFFER_SIZE만큼 읽은 내용에 개행이 포함되어 있다면 line에 개행 전까지의 라인을 반환하는 get_newline(), return_line()과 개행이후의 backup을 세팅해주는 set_backup()함수를 구현함. (test needed)

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,42 @@
 
 #include "get_next_line.h"
 
+int		set_backup(char **backup, int newline)
+{
+	char		*temp;
+	int			size;
+
+	if (ft_strlen(*backup + (newline + 1)) == 0)
+	{
+		free(*backup);
+		*backup = NULL;
+	}
+	else
+	{
+		temp = *backup;
+		size = ft_strlen(temp) - (newline + 1);
+		if (!(*backup = (char *)malloc(sizeof(char) * (size + 1))))
+			return (-1);
+		ft_strlcpy(*backup, &temp[newline + 1], (size + 1));
+		free(temp);
+		temp = NULL;
+	}
+	return (1);
+}
+
+int		return_line(char **backup, int newline, char **line)
+{
+	int			size;
+	char		*one_line;
+	
+	size = ft_strlen(*backup) + ft_strlen(*backup + newline);
+	if (!(one_line = (char *)malloc(sizeof(char) * (size + 1))))
+		return (-1);
+	ft_strlcpy(one_line, *backup, (size + 1));
+	*line = one_line;
+	return (set_backup(backup, newline));
+}
+
 int		append_backup(char **backup, char **buf, int read_size)
 {
 	char		*temp;
@@ -28,6 +64,21 @@ int		append_backup(char **backup, char **buf, int read_size)
 		free(temp);
 		temp = NULL;
 	}
+	return (0);
+}
+
+int		get_newline(char *str)
+{
+	int			newline;
+
+	newline = 0;
+	while (str[newline])
+	{
+		if (str[newline] == '\n')
+			return (newline);
+		newline++;
+	}
+	return (-1);
 }
 
 int		get_next_line(int fd, char **line)
@@ -35,6 +86,7 @@ int		get_next_line(int fd, char **line)
 	static char	*backup;
 	char		*buf;
 	int			read_size;
+	int			newline;
 
 	if (fd < 0 || line == NULL || BUFFER_SIZE < 1)
 		return (-1);
@@ -42,14 +94,16 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
+		buf[read_size] = '\0';
 		if (append_backup(&backup, &buf, read_size) < 0)
 			return (-1);
-		*line = ft_strdup(backup);
-		return (1);
+		if ((newline = get_newline(buf)) >= 0)
+			return (return_line(&backup, newline, line));
 	}
-	free(backup);
 	free(buf);
 	if (read_size < 0)
 		return (-1);
+	*line = ft_strdup(backup);
+	free(backup);
 	return (0);
 }
