a02074df4eaf948b05ff3330a3f7df66681c7f78
jna <vonreal20@gmail.com>
2021-01-22 05:47:17 +0900

    [header] #ifndef #endif before plz input spacegit add get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,21 @@
 /*   By: jna <jna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 00:11:58 by jna               #+#    #+#             */
-/*   Updated: 2021/01/22 00:11:58 by jna              ###   ########.fr       */
+/*   Updated: 2021/01/22 05:46:36 by jna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE = 1
-#endif
+# ifdef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
 
-int		get_newline(char **str);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strdup(char *str);
