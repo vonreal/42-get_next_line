fcd0a8ebd631d429cdaa7db5f3e517f80eb57ae5
vonrea <vonreal20@gmail.com>
2021-01-22 01:44:12 +0900

    backup에 읽어들인 내용을 이어붙히는 부분을 함수로 구현해도 작동하는가? (테스트 needed)

--- get_next_line.c
+++ get_next_line.c
@@ -12,11 +12,28 @@
 
 #include "get_next_line.h"
 
+void	append_backup(char **backup, char **buf, int read_size)
+{
+	char		*temp;
+
+	if (*backup == NULL)
+		*backup = ft_strdup(*buf);
+	else
+	{
+		temp = *backup;
+		if (!(*backup = (char *)malloc(sizeof(char) * (ft_strlen(temp) + read_size + 1))))
+			return (-1);
+		ft_strlcpy(*backup, temp, (ft_strlen(temp) + 1));
+		ft_strlcpy(*(*backup + ft_strlen(temp)), *buf, (ft_strlen(*buf) + 1));
+		free(temp);
+		temp = NULL;
+	}
+}
+
 int		get_next_line(int fd, char **line)
 {
 	static char	*backup;
 	char		*buf;
-	char		*temp;
 	int			read_size;
 
 	if (fd < 0 || line == NULL || BUFFER_SIZE < 1)
@@ -25,18 +42,7 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 	while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-		if (backup == NULL)
-			backup = ft_strdup(buf);
-		else
-		{
-			temp = backup;
-			if (!(backup = (char *)malloc(sizeof(char) * (ft_strlen(temp) + read_size + 1))))
-				return (-1);
-			ft_strlcpy(backup, temp, (ft_strlen(temp) + 1));
-			ft_strlcpy(&backup[ft_strlen(temp)], buf, (ft_strlen(buf) + 1));
-			free(temp);
-			temp = NULL;
-		}
+		append_backup(&backup, &buf, read_size);
 		*line = backup;
 		return (1);
 	}
